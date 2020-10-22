5a7b024714cc625162b224a0f3c89cb54e9e86ab
Furkan Kaya <8fkaya@gmail.com>
2020-10-22 12:50:11 +0300

    version 1 is done for big screens

--- lib/config/constants.dart
+++ lib/config/constants.dart
@@ -7,4 +7,10 @@ class Constants {
     stops: [0.1, 0.3, 0.7],
     colors: [Colors.deepOrangeAccent, Colors.purple, Colors.blue],
   );
+
+  static const instagram = "social_media/instagram.png";
+  static const linkedin = "social_media/linkedin.png";
+  static const twitter = "social_media/twitter.png";
+  static const mail = "social_media/mail.png";
+  static const logo = "logo.png";
 }
--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,7 +1,11 @@
 import 'dart:html';
 
 import 'package:emreyazar_com/config/constants.dart';
+import 'package:emreyazar_com/widgets/theme_inherited_widget.dart';
 import 'package:flutter/material.dart';
+
+// ignore: avoid_web_libraries_in_flutter
+import 'dart:html' as html;
 
 class HomePage extends StatefulWidget {
   HomePage({Key key}) : super(key: key);
@@ -14,134 +18,156 @@ class _HomePageState extends State<HomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: MediaQuery.of(context).size.width >= 500 ? bigScreen() : smallScreen(),
+      body: MediaQuery.of(context).size.width >= 500 ? bigScreen() : smallScreen("Furkan"),
     );
   }
 
   Widget bigScreen() {
     return Padding(
-      padding: EdgeInsets.only(left: 120, top: 75, right: 100, bottom: 75),
+      padding: EdgeInsets.only(left: 180, top: 90, right: 180, bottom: 80),
       child: Column(
         children: <Widget>[
           Row(
             mainAxisAlignment: MainAxisAlignment.spaceBetween,
             children: <Widget>[
-              Container(
-                height: 30,
-                width: 30,
-                color: Colors.white,
-              ),
-              Text(
-                "resume",
-                style: TextStyle(fontSize: 18, color: Colors.white70, fontFamily: 'Inconsolata', fontWeight: FontWeight.w700),
+              SizedBox(height: 65, child: Image.asset(Constants.logo)),
+              Row(
+                children: [
+                  FlatButton(
+                    hoverColor: Colors.white12,
+                    child: Text(
+                      "resume",
+                      style: TextStyle(fontSize: 20, color: Colors.white70, fontFamily: 'Inconsolata', fontWeight: FontWeight.w700),
+                    ),
+                    onPressed: () => {},
+                  ),
+                  Container(
+                    height: 30,
+                    width: 2,
+                    decoration: BoxDecoration(gradient: Constants.colorfulLine),
+                  ),
+                ],
               ),
             ],
           ),
-          SizedBox(
-            height: 150,
-          ),
-          Column(
-            crossAxisAlignment: CrossAxisAlignment.start,
-            children: [
-              Text(
-                "welcome.",
-                style: TextStyle(
-                  color: Colors.white54,
-                  fontFamily: 'Jura',
-                  fontWeight: FontWeight.w200,
-                  height: 0.9,
-                  fontSize: 75,
-                ),
-                textAlign: TextAlign.left,
-              ),
-              SizedBox(
-                height: 15,
-              ),
-              Row(
-                children: <Widget>[
-                  Row(
-                    children: [
-                      Container(
-                        height: 200,
-                        width: 2,
-                        decoration: BoxDecoration(gradient: Constants.colorfulLine),
+          Expanded(
+            child: Row(
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    Text(
+                      "welcome.",
+                      style: TextStyle(
+                        color: Colors.white54,
+                        fontFamily: 'Jura',
+                        fontWeight: FontWeight.w200,
+                        height: 0.9,
+                        fontSize: 85,
                       ),
-                      SizedBox(
-                        width: 10,
-                      ),
-                      Column(
-                        mainAxisAlignment: MainAxisAlignment.start,
-                        children: [
-                          Text(
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPraesent at auctor purus. Praesent et neque ex. \nNunc non est dapibus, aliquam ante sed, sollicitudin velit.\nCras eu lectus turpis. ",
-                            maxLines: 4,
-                            style: TextStyle(color: Colors.white70, fontSize: 15, height: 1.3, fontFamily: 'Inconsolata', fontWeight: FontWeight.w500),
-                            textAlign: TextAlign.left,
-                          ),
-                          SizedBox(
-                            height: 10,
-                          ),
-                          Text(
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPraesent at auctor purus. Praesent et neque ex. \nNunc non est dapibus, aliquam ante sed, sollicitudin velit.\nCras eu lectus turpis. ",
-                            maxLines: 4,
-                            style: TextStyle(color: Colors.white70, fontSize: 15, height: 1.3, fontFamily: 'Inconsolata', fontWeight: FontWeight.w500),
-                            textAlign: TextAlign.left,
-                          ),
-                        ],
-                      ),
-                    ],
-                  ),
-                  Container(
-                    color: Colors.white,
-                    height: 200,
-                    width: 200,
-                    child: Column(
-                      mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                      children: [
+                      textAlign: TextAlign.left,
+                    ),
+                    SizedBox(
+                      height: 15,
+                    ),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.center,
+                      children: <Widget>[
                         Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
                           children: [
                             Container(
-                              color: Colors.red,
-                              height: 15,
-                              width: 15,
+                              height: 200,
+                              width: 2,
+                              decoration: BoxDecoration(gradient: Constants.colorfulLine),
                             ),
-                            Container(
-                              color: Colors.red,
-                              height: 15,
-                              width: 15,
+                            SizedBox(
+                              width: 10,
                             ),
-                          ],
-                        ),
-                        Row(
-                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                          children: [
-                            Container(
-                              color: Colors.red,
-                              height: 15,
-                              width: 15,
-                            ),
-                            Container(
-                              color: Colors.red,
-                              height: 15,
-                              width: 15,
+                            Column(
+                              mainAxisAlignment: MainAxisAlignment.start,
+                              children: [
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPraesent at auctor purus. Praesent et neque ex. \nNunc non est dapibus, aliquam ante sed, sollicitudin velit.\nCras eu lectus turpis. ",
+                                  maxLines: 4,
+                                  style: TextStyle(color: Colors.white70, fontSize: 15, height: 1.3, fontFamily: 'Inconsolata', fontWeight: FontWeight.w500),
+                                  textAlign: TextAlign.left,
+                                ),
+                                SizedBox(
+                                  height: 10,
+                                ),
+                                Text(
+                                  "Lorem ipsum dolor sit amet, consectetur adipiscing elit.\nPraesent at auctor purus. Praesent et neque ex. \nNunc non est dapibus, aliquam ante sed, sollicitudin velit.\nCras eu lectus turpis. ",
+                                  maxLines: 4,
+                                  style: TextStyle(color: Colors.white70, fontSize: 15, height: 1.3, fontFamily: 'Inconsolata', fontWeight: FontWeight.w500),
+                                  textAlign: TextAlign.left,
+                                ),
+                              ],
                             ),
                           ],
                         ),
                       ],
                     ),
+                  ],
+                ),
+                Spacer(
+                  flex: 2,
+                ),
+                Container(
+                  height: 200,
+                  width: 200,
+                  child: Stack(
+                    children: [
+                      Positioned(
+                        left: 0,
+                        top: 0,
+                        child: socialMediaButtons(name: "instagram", iconPath: Constants.instagram, link: "https://www.instagram.com/razayerme/"),
+                      ),
+                      Positioned(
+                        right: 0,
+                        top: 0,
+                        child: socialMediaButtons(name: "twitter", iconPath: Constants.twitter, link: "https://twitter.com/razayerme/"),
+                      ),
+                      Positioned(
+                        left: 0,
+                        bottom: 0,
+                        child: socialMediaButtons(name: "linkedin", iconPath: Constants.linkedin, link: "https://www.linkedin.com/in/eyazar8/"),
+                      ),
+                      Positioned(
+                        right: 0,
+                        bottom: 0,
+                        child: socialMediaButtons(name: "mail", iconPath: Constants.mail, link: "me@emreyazar.com"),
+                      ),
+                    ],
                   ),
-                ],
-              ),
-            ],
+                ),
+                Spacer(),
+              ],
+            ),
           ),
         ],
       ),
     );
   }
 
-  Widget smallScreen() {
-    return Text("small");
+  Widget smallScreen(String test) {
+    return Text(test);
+  }
+
+  Widget socialMediaButtons({String name, String link, String iconPath}) {
+    return IconButton(
+      iconSize: 65,
+      icon: Image.asset(iconPath),
+      onPressed: () => html.window.open(link, 'emreyazar'),
+    );
+    /*
+    FlatButton.icon(
+      hoverColor: ThemeSwitcher.of(context).isDarkModeOn ? Colors.indigo[900] : Colors.black12,
+      splashColor: ThemeSwitcher.of(context).isDarkModeOn ? Colors.black : Colors.white,
+      icon: SizedBox(width: 26, height: 26, child: Image.asset(iconPath)),
+      onPressed: () => html.window.open(link, 'emreyazar'),
+    );*/
   }
 }
 
